9752ab02b0db89188b2da47970077d5337f4f1b3
Fernando Jose Santiago Tulley <55557447+FerjoSantiago@users.noreply.github.com>
2026-03-24 10:30:47 -0600

    Add function to load articles into a DataFrame

--- articulos.py
+++ articulos.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+def cargar_articulos():
+    data = {
+        "Articulo": ["Limpiador Multiusos", "Desinfectante", "Jabón Líquido"],
+        "Max_Mensual": [500, 300, 700],
+        "Min_Mensual": [200, 120, 300],
+        "Costo_Unitario": [45.0, 60.0, 25.0]
+    }
+    return pd.DataFrame(data)
