4124c4a6626488862aedd6b28d6101c158346146
Fernando Jose Santiago Tulley <55557447+FerjoSantiago@users.noreply.github.com>
2026-03-24 10:31:49 -0600

    Add inventory loading function with sample data

--- inventarios.py
+++ inventarios.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import numpy as np
 
+def cargar_inventario():
+    data = {
+        "Sucursal": ["0002", "0003", "0004"],
+        "Limpiador Multiusos": [180, 260, 340],
+        "Desinfectante": [120, 200, 350],
+        "Jabón Líquido": [400, 500, 800]
+    }
+    return pd.DataFrame(data)
